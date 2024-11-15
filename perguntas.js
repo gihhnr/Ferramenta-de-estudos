baae8b809b2f1851a67c71c33fd0c8d921daa899
gihhnr <169050900+gihhnr@users.noreply.github.com>
2024-11-14 21:53:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'O maior país do mundo é a Rússia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O rio mais longo do mundo é o Rio Nilo'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais células possuem núcleo?',
+    'As células que possuem núcleo são as eucariontes'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos litros de sangue uma pessoa tem?',
+    'Uma pessoa tem entre 4 a 6 litros de sangue no corpo'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os 5 reinos que existem na biologia?',
+    'Os reinos que existem na biologia são: Reino Animal; Vegetal; Protista; Fungi e Monera'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "bom dia" em inglês?',
+    'Se diz "good morning"'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "obrigado" em inglês?',
+    'Se diz "thank you"'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "quantos anos você tem" em inglês?',
+    'Se diz "how old are you"'
+)
